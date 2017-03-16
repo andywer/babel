6954513bf53d1616d05b00abc190287f36f5df88
Andy Wermke <andy@dev.next-step-software.com>
2017-03-16 19:11:33 +0100

    Use findSiblingDeclarationNode() from dctypes-to-flow plugin

--- packages/babel-plugin-transform-dctypes-comments/src/index.js
+++ packages/babel-plugin-transform-dctypes-comments/src/index.js
@@ -1,4 +1,5 @@
 const syntaxDCTypes = require('@andywer/babel-plugin-syntax-dctypes')
+const { findSiblingDeclarationNode } = require('@andywer/babel-plugin-transform-dctypes-to-flow')
 
 module.exports = babelPluginDCTypeComments
 
@@ -24,45 +25,6 @@ function babelPluginDCTypeComments () {
       }
     }
   }
-}
-
-function findSiblingDeclarationNode (path, idNode) {
-  if (!path.inList) {
-    throw path.buildCodeFrameError(`Type declaration of '${idNode.name}' has no sibling declarations. '${idNode.name}' should be declared in the same scope.`)
-  }
-
-  const siblingNodes = path.container.filter(node => node !== path.node)
-
-  return findInArray(siblingNodes, sibling => {
-    if (sibling.type === 'VariableDeclaration') {
-      const matchingDecl = sibling.declarations.find(decl => decl.id && decl.id.name === idNode.name)
-      return matchingDecl || null
-    } else {
-      return sibling.id && sibling.id.name === idNode.name
-        ? sibling
-        : null
-    }
-  })
-}
-
-/**
- * Works like `array.find(finder)` but returns the result of the first `finder`
- * call that did find something. Allows extraction of nested data.
- */
-function findInArray(array, finder) {
-  let match = null
-
-  array.find((item, key) => {
-    const result = finder(item, key)
-    if (result) {
-      match = result
-      return true     // break loop
-    } else {
-      return false    // continue loop
-    }
-  })
-
-  return match
 }
 
 function generateComment(path) {
